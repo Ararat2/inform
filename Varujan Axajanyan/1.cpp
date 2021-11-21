861b4efa1668bd4503d3b5e9461035cfe6f88b5d
Ararat2 <araratm@boomte.ch>
2021-11-21 17:45:05 +0400

    Varujan Axajanyan

---
+++ Varujan Axajanyan/1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int random(int max) {
+  int min = -max;
+
+  return min + rand() % (( max + 1 ) - min);
+}
+
+int main() {
+    int n;
+    
+    vector<int> X;
+    vector<int> Y;
+
+    // Get n
+    do {
+        cout << "Type n: ";
+        cin >> n;
+    } while (n <= 0);
+
+    int numbers[n][n];
+
+    // Fill X
+    for (int i = 0; i < n; i++) X.push_back(random(n));
+
+    // Fill Y
+    for (int i = 0; i < X.size(); i++) {
+        if (i % 2 == 0) {
+         Y.push_back(X[i]);
+        }
+    }
+
+    // Show Y data
+    for (int i: Y) cout << i << ", ";
+
+    return 0;
+}
---
+++ Varujan Axajanyan/2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int random(int max) {
+    int min = -max;
+
+    return min + rand() % (( max + 1 ) - min);
+}
+
+int main() {
+    int n;
+    int k;
+    int gtk_count = 0;
+
+    // Get n and k
+    do {
+        cout << "Type n: ";
+        cin >> n;
+    } while (n <= 0);
+
+    cout << "Typw k: ";
+    cin >> k;
+
+    int numbers[n][n];
+
+    // Fill numbers array
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            numbers[i][j] = random(n);
+        }
+    }
+
+    // Get items count
+    for (int i = 0; i < n; i++) {
+        int item = numbers[i][i];
+
+        if (item < 0) {
+            item *= -1;
+        }
+
+        if (item > k) {
+            gtk_count++;
+        }
+    }
+
+    // Show result
+    cout << "Result: " << gtk_count << endl;
+
+    return 0;
+}
